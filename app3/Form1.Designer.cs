dd6f0fb0ce6bc41499705489afb944d528e45c7c
JakobMenBedre <jakobrasm@viken.no>
2023-11-03 08:25:37 +0100

    Add project files.

---
+++ app3/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace app3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addisjon = new System.Windows.Forms.Button();
+            this.subtraksjon = new System.Windows.Forms.Button();
+            this.multiplikasjon = new System.Windows.Forms.Button();
+            this.divisjon = new System.Windows.Forms.Button();
+            this.resultat = new System.Windows.Forms.Label();
+            this.input1 = new System.Windows.Forms.TextBox();
+            this.input2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.desimalerlabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // addisjon
+            // 
+            this.addisjon.Location = new System.Drawing.Point(538, 83);
+            this.addisjon.Name = "addisjon";
+            this.addisjon.Size = new System.Drawing.Size(126, 42);
+            this.addisjon.TabIndex = 0;
+            this.addisjon.Text = "+";
+            this.addisjon.UseVisualStyleBackColor = true;
+            this.addisjon.Click += new System.EventHandler(this.addisjon_Click);
+            // 
+            // subtraksjon
+            // 
+            this.subtraksjon.Location = new System.Drawing.Point(538, 131);
+            this.subtraksjon.Name = "subtraksjon";
+            this.subtraksjon.Size = new System.Drawing.Size(126, 42);
+            this.subtraksjon.TabIndex = 1;
+            this.subtraksjon.Text = "-";
+            this.subtraksjon.UseVisualStyleBackColor = true;
+            this.subtraksjon.Click += new System.EventHandler(this.subtraksjon_Click);
+            // 
+            // multiplikasjon
+            // 
+            this.multiplikasjon.Location = new System.Drawing.Point(538, 179);
+            this.multiplikasjon.Name = "multiplikasjon";
+            this.multiplikasjon.Size = new System.Drawing.Size(126, 42);
+            this.multiplikasjon.TabIndex = 2;
+            this.multiplikasjon.Text = "*";
+            this.multiplikasjon.UseVisualStyleBackColor = true;
+            this.multiplikasjon.Click += new System.EventHandler(this.multiplikasjon_Click);
+            // 
+            // divisjon
+            // 
+            this.divisjon.Location = new System.Drawing.Point(538, 227);
+            this.divisjon.Name = "divisjon";
+            this.divisjon.Size = new System.Drawing.Size(126, 42);
+            this.divisjon.TabIndex = 3;
+            this.divisjon.Text = "/";
+            this.divisjon.UseVisualStyleBackColor = true;
+            this.divisjon.Click += new System.EventHandler(this.divisjon_Click);
+            // 
+            // resultat
+            // 
+            this.resultat.AutoSize = true;
+            this.resultat.Location = new System.Drawing.Point(50, 227);
+            this.resultat.Name = "resultat";
+            this.resultat.Size = new System.Drawing.Size(13, 13);
+            this.resultat.TabIndex = 4;
+            this.resultat.Text = "0";
+            // 
+            // input1
+            // 
+            this.input1.Location = new System.Drawing.Point(53, 83);
+            this.input1.Name = "input1";
+            this.input1.Size = new System.Drawing.Size(100, 20);
+            this.input1.TabIndex = 5;
+            // 
+            // input2
+            // 
+            this.input2.Location = new System.Drawing.Point(53, 131);
+            this.input2.Name = "input2";
+            this.input2.Size = new System.Drawing.Size(100, 20);
+            this.input2.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.comboBox1.Location = new System.Drawing.Point(538, 294);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Sorted = true;
+            this.comboBox1.TabIndex = 7;
+            // 
+            // desimalerlabel
+            // 
+            this.desimalerlabel.AutoSize = true;
+            this.desimalerlabel.Location = new System.Drawing.Point(339, 294);
+            this.desimalerlabel.Name = "desimalerlabel";
+            this.desimalerlabel.Size = new System.Drawing.Size(80, 13);
+            this.desimalerlabel.TabIndex = 8;
+            this.desimalerlabel.Text = "Antall desimaler";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.desimalerlabel);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.input2);
+            this.Controls.Add(this.input1);
+            this.Controls.Add(this.resultat);
+            this.Controls.Add(this.divisjon);
+            this.Controls.Add(this.multiplikasjon);
+            this.Controls.Add(this.subtraksjon);
+            this.Controls.Add(this.addisjon);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addisjon;
+        private System.Windows.Forms.Button subtraksjon;
+        private System.Windows.Forms.Button multiplikasjon;
+        private System.Windows.Forms.Button divisjon;
+        private System.Windows.Forms.Label resultat;
+        private System.Windows.Forms.TextBox input1;
+        private System.Windows.Forms.TextBox input2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label desimalerlabel;
+    }
+}
+
---
+++ app3/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace app3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addisjon_Click(object sender, EventArgs e) //når man trykker på knappen som heter addisjon skjer det under
+            
+        {
+            
+            try { //try-catch mer eller mindre gjør slik at errors ikke bare ødelegger hele programmet, men at den bare stopper det den holder på med hvis det er noe som ikke funker (try)og så lar deg kjøre en kode som skjer da (catch).
+                int combobox = Convert.ToInt32(comboBox1.Text); //lager integer som heter combobox som er verdien til comboBox1.Text men som en integer.
+                if (combobox <= -1)
+                {
+                    MessageBox.Show("broder du kan ikke ha negative antall desimaltall. ødalagt as."); //sjekker om at combobox sin verdi er mindre enn 0. hvis den er så kjefter programmet på deg og kaller deg for ødelagt.
+                }
+
+                double verdi1 = Convert.ToDouble(input1.Text); //lager en double som heter verdi1 og verdien dens er input1.Text men gjort om til en double med Convert.ToDouble 
+                double verdi2 = Convert.ToDouble(input2.Text); //samme som over men med verdi2 og input2.Text
+
+                double res = Math.Round(verdi1 + verdi2, combobox); //lager en til double variabel som heter res. denne har verdien verdi1 + verdi2, men at den rundes opp til to desimaler med Math.Round([verdi], [antall desimaltall])
+                resultat.Text = res.ToString(); //gjør gir resultat.Text verdien res are at den er en string.
+                
+            }
+            catch (FormatException ex){
+                resultat.Text = "Her har du skrevet noe galt:" + ex.Message;//her fanger program opp ting som har gått galt, sier at det har gått noe galt, og hva det er som er feil (på engelsk da) også så er ex error-messagen
+            }
+            catch (Exception ex)
+            {
+                resultat.Text = "Her har noe gått galt:" + ex.Message; //samme som over
+            }
+        }
+
+        private void subtraksjon_Click(object sender, EventArgs e) 
+        {
+
+            try
+            {
+
+                int combobox = Convert.ToInt32(comboBox1.Text);
+                if (combobox <= -1)
+                {
+                    MessageBox.Show("broder du kan ikke ha negative antall desimaltall. ødalagt as.");//alt det over men med subtraksjon
+                }
+
+                double verdi1 = Convert.ToDouble(input1.Text);
+                double verdi2 = Convert.ToDouble(input2.Text);
+
+                double res = Math.Round(verdi1 - verdi2, combobox);
+                resultat.Text = res.ToString();
+            }
+            catch (FormatException ex)
+            {
+                resultat.Text = "Her har du skrevet noe galt:" + ex.Message;
+            }
+            catch (Exception ex)
+            {
+                resultat.Text = "Her har noe gått galt:" + ex.Message;
+            }
+        }
+
+        private void multiplikasjon_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                
+                int combobox = Convert.ToInt32(comboBox1.Text);
+                if (combobox <= -1)
+                {
+                    MessageBox.Show("broder du kan ikke ha negative antall desimaltall. ødalagt as.");//alt det over men med multiplikasjon
+                }
+
+                double verdi1 = Convert.ToDouble(input1.Text); 
+                double verdi2 = Convert.ToDouble(input2.Text); 
+
+                double res = Math.Round(verdi1 * verdi2, combobox); 
+                resultat.Text = res.ToString(); 
+            }
+            catch (FormatException ex)
+            {
+                resultat.Text = "Her har du skrevet noe galt:" + ex.Message; 
+            }
+            catch (Exception ex)
+            {
+                resultat.Text = "Her har noe gått galt:" + ex.Message; 
+            }
+        }
+
+        private void divisjon_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                
+                int combobox = Convert.ToInt32(comboBox1.Text);
+                if (combobox <= -1)
+                {
+                    MessageBox.Show("broder du kan ikke ha negative antall desimaltall. ødalagt as."); // alt det over bare med divisjon og en til ting
+                }
+
+                double verdi1 = Convert.ToDouble(input1.Text); 
+                double verdi2 = Convert.ToDouble(input2.Text);
+
+                double res = Math.Round(verdi1 / verdi2, combobox); 
+                resultat.Text = res.ToString(); 
+                if (verdi2 == 0)
+                {
+                    MessageBox.Show("BRODER HVEM TROR DU AT DU ER MED AT DU PRØVER Å DELE PÅ 0!?!"); //om du prøver å dele på 0 kjefter programmet på deg med en popup.
+                } 
+            }
+            catch (FormatException ex)
+            {
+                resultat.Text = "Her har du skrevet noe galt:" + ex.Message; 
+            }
+            catch (Exception ex)
+            {
+                resultat.Text = "Her har noe gått galt:" + ex.Message; 
+            }
+        }
+    }
+}
